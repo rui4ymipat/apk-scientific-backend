f002be7e28bcc7ec641cb4e0c70d14d810bec4a0
rui4ymipat <59613118+rui4ymipat@users.noreply.github.com>
2022-11-16 10:08:22 +0700

    new func

--- functions/index.js
+++ functions/index.js
@@ -2,71 +2,26 @@ const functions = require("firebase-functions");
 const regions = "asia-southeast2";
 const cors = require("cors")({ origin: true });
 const admin = require("firebase-admin");
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL:
+    "https://apk-scientific-default-rtdb.asia-southeast1.firebasedatabase.app/",
+});
 const category_collection = admin.firestore().collection("categoy");
 const product_collection = admin.firestore().collection("product");
+const category_list = admin
+  .firestore()
+  .collection("masterData")
+  .doc("category");
 
 exports.example = functions.region(regions).https.onRequest((req, res) => {
   cors(req, res, () => {
     cors(req, res, () => {
       const method = req.method;
       if (method === "GET") {
-        // Get all category
-        category_collection
-          .get()
-          .then((snapshot) => {
-            const category = [];
-            snapshot.forEach((doc) => {
-              category.push({
-                id: doc.id,
-                ...doc.data(),
-              });
-            });
-            return res.status(200).json(category);
-          })
-          .catch((error) => {
-            return res.status(500).json({ error: error.message });
-          });
       } else if (method === "POST") {
-        // Create new category
-        const { name } = req.body;
-        const d = new Date();
-        category_collection
-          .doc(`${d.getTime()}`)
-          .set({
-            ...req.body,
-            created_at: d.getTime(),
-          })
-          .then((doc) => {
-            return res.status(200).json({ id: doc.id });
-          })
-          .catch((error) => {
-            return res.status(500).json({ error: error.message });
-          });
       } else if (method === "PUT") {
-        // Update category
-        const { id } = req.body;
-        category_collection
-          .doc(id)
-          .update(req.body)
-          .then(() => {
-            return res.status(200).json({ id });
-          })
-          .catch((error) => {
-            return res.status(500).json({ error: error.message });
-          });
       } else if (method === "DELETE") {
-        // Delete category
-        const { id } = req.query;
-        category_collection
-          .doc(id)
-          .delete()
-          .then(() => {
-            return res.status(200).json({ id });
-          })
-          .catch((error) => {
-            return res.status(500).json({ error: error.message });
-          });
       } else {
         res.status(405).send("Method Not Allowed");
       }
@@ -123,6 +78,98 @@ exports.manage_category = functions
       } else {
         res.status(405).send("Method Not Allowed");
       }
+    });
+  });
+
+exports.list_category = functions
+  .region(regions)
+  .https.onRequest((req, res) => {
+    cors(req, res, () => {
+      cors(req, res, () => {
+        const method = req.method;
+        if (method === "GET") {
+          category_list.get().then((snapshot) => {
+            res.status(200).send(snapshot.data().category);
+          });
+        } else if (method === "POST") {
+          category_list.set({
+            category: req.body,
+          });
+          res.status(200).send("success");
+        } else if (method === "PUT") {
+          category_list.set({
+            category: req.body,
+          });
+          res.status(200).send("success");
+        } else if (method === "DELETE") {
+          category_list.set({}).then((doc) => {
+            res.status(200).send(doc.id);
+          });
+        } else {
+          res.status(405).send("Method Not Allowed");
+        }
+      });
+    });
+  });
+
+exports.product_list = functions.region(regions).https.onRequest((req, res) => {
+  cors(req, res, () => {
+    cors(req, res, () => {
+      const method = req.method;
+      console.log(method);
+      if (method === "GET") {
+        const { category } = req.query;
+        if (category) {
+          product_collection
+            .where("category", "in", [category])
+            .get()
+            .then((snapshot) => {
+              const data = [];
+              snapshot.forEach((doc) => {
+                data.push({
+                  id: doc.id,
+                  ...doc.data(),
+                });
+              });
+              res.status(200).send(data);
+            });
+        } else {
+          product_collection.get().then((snapshot) => {
+            const data = [];
+            snapshot.forEach((doc) => {
+              data.push({
+                id: doc.id,
+                ...doc.data(),
+              });
+            });
+            res.status(200).send(data);
+          });
+        }
+      } else {
+        res.status(405).send("Method Not Allowed");
+      }
+    });
+  });
+});
+
+exports.get_product_by_id = functions
+  .region(regions)
+  .https.onRequest((req, res) => {
+    cors(req, res, () => {
+      cors(req, res, () => {
+        const method = req.method;
+        if (method === "GET") {
+          const { id } = req.query;
+          product_collection
+            .doc(id)
+            .get()
+            .then((snapshot) => {
+              res.status(200).send(snapshot.data());
+            });
+        } else {
+          res.status(405).send("Method Not Allowed");
+        }
+      });
     });
   });
 
